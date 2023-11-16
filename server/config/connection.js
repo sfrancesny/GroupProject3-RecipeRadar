57fb74b18f69d9b2677c532462a7a801822a759e
sfrancesny <134027005+sfrancesny@users.noreply.github.com>
2023-11-16 14:56:57 -0500

    altered connection.js so it involves env variables

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://sonianyenkan80:BKCFPV81@cluster0.4staqrz.mongodb.net/recipe-radar-db?retryWrites=true&w=majority');
+// Use an environment variable for your MongoDB URI
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+    console.error('No MongoDB connection string. Set MONGODB_URI environment variable.');
+    process.exit(1);
+}
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+})
+.then(() => console.log('MongoDB connected successfully.'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 module.exports = mongoose.connection;
