b3eabaf399ef2a8670ed17d99df921de62200e3f
sfrancesny <134027005+sfrancesny@users.noreply.github.com>
2023-11-16 09:11:42 -0500

    updated README alterred useAuth file / created handle functions

--- README.md
+++ README.md
@@ -1,18 +1,37 @@
- #  Group Project 3 - RecipeRadar 
-<!-- On-the-job ticket or feature request Challenges -->
+# Group Project 3 - RecipeRadar
 
-## Description 
+## Description
+Welcome to RecipeRadar, a culinary delight for every home cook and food enthusiast! Our application is designed to bring the world of recipes to your fingertips. Whether you're an experienced chef or just starting out in the kitchen, RecipeRadar offers a user-friendly platform to explore, create, and share your favorite recipes.
 
+## Key Features
+- **Explore Recipes**: Dive into a variety of cuisines and find your next favorite dish.
+- **Create and Share**: Have a recipe you're proud of? Easily add it to our database and let others enjoy your culinary creations.
+- **Update and Delete**: Keep your recipes up to date with our easy-to-use editing tools. **CURRENTLY UNDER CONSTRUCTION**
+- **Personalized Experience**: Save your favorite recipes and have them accessible anytime.
+- **Responsive Design**: Whether on a desktop, tablet, or mobile, RecipeRadar is fully responsive and easy to navigate.
 
-## Usage 
+## Usage
+1. **Explore Recipes**: Browse through a collection of recipes from various cuisines.
+2. **Add a Recipe**: Logged-in users can add new recipes by filling out a simple form.
+3. **Edit and Delete**: Users can update their recipes or remove them from the database.
+4. **Personal Home Page**: Access your saved and submitted recipes in one place.
 
+## Screenshots
+Here's a glimpse of what RecipeRadar has to offer:
 
+![RecipeRadar Home Page](<screenshot link>)
 
- ## Screenshots
-<img src=""/> 
+## Link to Deployed Application
+Experience RecipeRadar now: [RecipeRadar Live](<live link>)
 
-## Link to Deployed Application 
+## Contributors
+This project is brought to you by a team of dedicated developers:
+- **[James Kawam](https://github.com/kawam001)**
 
-Links:
+## Tech Stack
+- Frontend: React, Apollo Client
+- Backend: Node.js, Express.js, GraphQL, MongoDB
+- Deployment: Heroku
 
-## Contributors 
+## Feedback and Contributions
+Your feedback is valuable to us! If you'd like to contribute or have any suggestions, feel free to open an issue
--- client/src/pages/homePage.jsx
+++ client/src/pages/homePage.jsx
@@ -1,13 +1,60 @@
 // client/src/pages/HomePage.jsx
 import React, { useState } from 'react';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { GET_RECIPES } from '../graphql/queries';
+import { UpdateRecipe, DeleteRecipe } from '../graphql/mutations';
+import UpdateRecipeForm from '../components/UpdateRecipeForm'; 
+import Modal from '../components/Modal';
 import './Home.css'; 
 
 const HomePage = () => {
   const { loading, error, data } = useQuery(GET_RECIPES);
   const [searchTerm, setSearchTerm] = useState('');
+  const currentUsername = localStorage.getItem('username'); 
 
+  const [updateRecipeMutation] = useMutation(UpdateRecipe);
+  const [deleteRecipeMutation] = useMutation(DeleteRecipe);
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleUpdateRecipe = (recipe) => {
+    console.log(`Update recipe with ID: ${recipe._id}`);
+    setSelectedRecipe(recipe);
+    setShowModal(true);
+  };
+  
+  const handleModalClose = () => {
+    setShowModal(false);
+    setSelectedRecipe(null);
+  };
+
+  const handleFormSubmit = (updatedData) => {
+    updateRecipeMutation({
+      variables: { _id: selectedRecipe._id, updates: updatedData },
+      refetchQueries: [{ query: GET_RECIPES }],
+    })
+    .then(response => {
+      console.log('Recipe updated:', response.data.updateRecipe);
+      setShowModal(false);
+    })
+    .catch(err => {
+      console.error('Error updating recipe:', err);
+    });
+  };
+
+  const handleDeleteRecipe = (id) => {
+    deleteRecipeMutation({
+      variables: { _id: id },
+      refetchQueries: [{ query: GET_RECIPES }],
+    })
+    .then(response => {
+      console.log('Recipe deleted:', response.data.deleteRecipe);
+    })
+    .catch(err => {
+      console.error('Error deleting recipe:', err);
+    });
+  };
+  
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
@@ -19,24 +66,22 @@ const HomePage = () => {
 
   // Filter recipes if there's a search term, otherwise show all recipes
   const displayedRecipes = searchTerm
-    ? recipes.filter((recipe) =>
-        recipe.title.toLowerCase().includes(searchTerm)
-      )
+    ? recipes.filter((recipe) => recipe.title.toLowerCase().includes(searchTerm))
     : recipes;
 
-    return (
-      <div className="home-container">
-        <div className="search-container">
-          <h1>Search For Your Favorite Recipe</h1>
-          <input 
-            type="text" 
-            className="search-input"
-            placeholder="Search by name" 
-            value={searchTerm}
-            onChange={handleSearchChange} 
-          />
-        </div>
-        <h1>Featured Recipes</h1>
+  return (
+    <div className="home-container">
+      <div className="search-container">
+        <h1>Search For Your Favorite Recipe</h1>
+        <input 
+          type="text" 
+          className="search-input"
+          placeholder="Search by name" 
+          value={searchTerm}
+          onChange={handleSearchChange} 
+        />
+      </div>
+      <h1>Featured Recipes</h1>
       <div className="recipes-container">
         {displayedRecipes.map((recipe) => (
           <div className="recipe-card" key={recipe._id}>
@@ -47,10 +92,21 @@ const HomePage = () => {
             <p className="recipe-servings">Servings: {recipe.servings}</p>
             <p className="recipe-ingredients">Ingredients: {recipe.ingredients.join(', ')}</p>
             <p className="recipe-instructions">Instructions: {recipe.instructions.join(', ')}</p>
-           <p className="recipe-author">Author: {recipe.author}</p> 
-           </div>
+            <p className="recipe-author">Author: {recipe.author}</p>
+            {currentUsername === recipe.author && (
+              <div className="recipe-actions">
+              <button className='update-button' onClick={() => handleUpdateRecipe(recipe._id)}>Update</button>
+              <button className='delete-button' onClick={() => handleDeleteRecipe(recipe._id)}>Delete</button>
+              </div>
+            )}
+          </div>
         ))}
       </div>
+      {showModal && (
+        <Modal show={showModal} onClose={handleModalClose}>
+          <UpdateRecipeForm recipe={selectedRecipe} onSubmit={handleFormSubmit} />
+        </Modal>
+      )}
     </div>
   );
 };
